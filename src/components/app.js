818f20c72d5aa816809cb50a4165c42a189634d9
dave doolin <david.doolin@gmail.com>
2018-07-14 13:04:44 -0700

    video 53, search bar mirroring input

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
+import SearchBar from '../containers/search_bar';
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
