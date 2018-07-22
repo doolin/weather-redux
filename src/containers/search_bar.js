a9f5a1ab3ab5d49e46441aa6d36ba008ab47e29a
dave doolin <david.doolin@gmail.com>
2018-07-22 09:45:00 -0700

    video 58, this commit has a bug

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class SearchBar extends Component {
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = { term: '' }
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -16,6 +21,8 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event) {
        event.preventDefault();
+       this.props.fetchWeather(this.state.term);
+       this.setState({ term: '' });
     }
 
     render() {
@@ -34,3 +41,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);    
