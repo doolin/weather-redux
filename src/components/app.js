59dc28dd6e08cf2128a473b4f9054c97a0b14b87
dave doolin <david.doolin@gmail.com>
2018-08-11 11:25:35 -0700

    video 61 connect to weather list

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
